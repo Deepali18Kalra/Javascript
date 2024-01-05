e15a1b92180f41ae7bac6ed83edb183755b9b479
Deepali18Kalra <114229530+Deepali18Kalra@users.noreply.github.com>
2024-01-05 17:45:51 +0000

    understanding let,const,var&console.table([])

---
+++ basics/variables.js
@@ -0,0 +1,13 @@
+const account_Id = 900027
+let account_Email = "deepali@gmail.com"
+var account_Password = "12563"
+account_City = "Delhi"
+
+// account_Id = 33 // not allowed
+account_Email = "d123@gmail.com"
+account_Password = "8999"
+account_City = "Udaipur"
+let accountAge;
+console.log(account_Id);
+
+console.table([account_Id, account_Email, account_Password, account_City, accountAge])
