6eda532a00bed20772650bd590f9cd1e60235783
Deepali18Kalra <114229530+Deepali18Kalra@users.noreply.github.com>
2024-01-05 19:19:25 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,20 @@
+"use strict" //treat all js code as newer js
+
+// alert("hello") // we are using node js and this syntax works in browser
+
+let name = "Deepali";
+let age = 21;
+let isLoggedIn = false;
+
+// number => numeric value
+// bigint => stores large numeric values
+// string => holds name or other string value like city name state name
+// boolean => stores true false value
+// null => empty value, used if nothing is returned, it is a standalone value
+// undefined => variable not defined yet
+// symbol => identifies unique components
+
+console.log(typeof age);
+console.log(typeof name);
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined because undefined is a type in js
